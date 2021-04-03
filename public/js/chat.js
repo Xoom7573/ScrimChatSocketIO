e1662a7a0eb5fd407c8d88c15f4fbf5af8de65ee
Xoom7573 <66717268+Xoom7573@users.noreply.github.com>
2021-04-03 11:33:56 +0200

    pretifie

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,9 +3,9 @@ var socket = io.connect('http://localhost:4000');
 
 // Query DOM
 var message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
-      btn = document.getElementById('send'),
-      output = document.getElementById('output');
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output');
 
 // Emit events
 btn.addEventListener('click', function(){
